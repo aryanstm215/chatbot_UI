731193138eb77c780810189c14c089e7e0926592
aryanstm215 <aryanagrahari1001@gmail.com>
2024-06-05 05:06:09 +0000

    050624_commited

--- Translation_model_practise.py
+++ Translation_model_practise.py
@@ -18,19 +18,23 @@ def Translate_Text(user_text, target_language):
         raise ValueError("Hugging Face API token not found in environment variables.")
 
     prompt_template = """
-           **Input Text:**
-        `{user_text}`
-    **Target Language:**
-        `{target_language}`
+    Translate the following text from English to {target_language}:
+
+    English: {user_text}
+    Translation:
     """
 
     prompt = PromptTemplate.from_template(prompt_template)
     chain = LLMChain(
-        llm=HuggingFaceHub(repo_id='mistralai/Mistral-7B-Instruct-v0.2', model_kwargs={'temperature':0.5 , 'max_new_tokens': 250}, huggingfacehub_api_token=api_token),
+        llm=HuggingFaceHub(repo_id='mistralai/Mistral-7B-Instruct-v0.2', model_kwargs={'temperature': 0.5, 'max_new_tokens': 50}, huggingfacehub_api_token=api_token),
         prompt=prompt
     )
     result = chain.run(user_text=user_text, target_language=target_language)
-    return result
+
+    # Parse the result to extract only the translation
+    translation = result.split("Translation:")[1].strip().split('\n')[0]
+    
+    return f"**Input Text:** `{user_text}`\n**Target Language:** `{target_language}`\n**Translation:** `{translation}`"
 
 def display_message(chat_message):
     role = chat_message["role"]
@@ -55,7 +59,7 @@ for chat_message in st.session_state.chat_history:
 # Input from user
 with st.form("user_input_form"):
     user_input = st.text_input("You:")
-    target_language = st.selectbox("Select target language", ["English", "Spanish", "French", "German", "Italian", "Chinese", "Japanese", "Russian"])
+    target_language = st.selectbox("Select target language", ["English", "Spanish", "French", "German", "Italian", "Chinese", "Japanese", "Russian","hindi","Malayalam"])
     submit_button = st.form_submit_button("Submit")
 
 # If user submits the form
